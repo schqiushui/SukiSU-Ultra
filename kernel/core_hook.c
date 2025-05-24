63f26e6b60f0d86b4174a076bf6020cb5b77b49c
backslashxx <118538522+backslashxx@users.noreply.github.com>
2025-05-24 19:31:37 +0800

    kernel: core_hook: enable path_umount for kernel >= 5.9 or with KSU_HAS_PATH_UMOUNT
    Use version check to allow path_umount on newer kernels,
    while keeping manual override for older ones.
    
    Signed-off-by: backslashxx <118538522+backslashxx@users.noreply.github.com>

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -23,8 +23,8 @@
 
 #include <linux/fs.h>
 #include <linux/namei.h>
-#ifndef KSU_HAS_PATH_UMOUNT
-#include <linux/syscalls.h> // sys_umount
+#if !(LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0)) && !defined(KSU_HAS_PATH_UMOUNT) 
+#include <linux/syscalls.h> // sys_umount (<4.17) & ksys_umount (4.17+)
 #endif
 
 #ifdef MODULE
@@ -998,11 +998,11 @@ static bool should_umount(struct path *path)
 #endif
 }
 
-#ifdef KSU_HAS_PATH_UMOUNT
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0) || defined(KSU_HAS_PATH_UMOUNT)
 static void ksu_path_umount(const char *mnt, struct path *path, int flags)
 {
-	int err = path_umount(path, flags);
-	pr_info("%s: path: %s ret: %d\n", __func__, mnt, err);
+	int ret = path_umount(path, flags);
+	pr_info("%s: path: %s ret: %d\n", __func__, mnt, ret);
 }
 #else
 // TODO: Search a way to make this works without set_fs functions
@@ -1011,7 +1011,7 @@ static void ksu_sys_umount(const char *mnt, int flags)
 	char __user *usermnt = (char __user *)mnt;
 	mm_segment_t old_fs;
 	int ret; // although asmlinkage long
-	
+
 	old_fs = get_fs();
 	set_fs(KERNEL_DS);
 #if LINUX_VERSION_CODE >= KERNEL_VERSION(4, 17, 0)
@@ -1020,7 +1020,7 @@ static void ksu_sys_umount(const char *mnt, int flags)
 	ret = sys_umount(usermnt, flags); // cuz asmlinkage long sys##name
 #endif
 	set_fs(old_fs);
-	pr_info("%s: path: %s ret: %d \n", __func__, usermnt, ret);
+	pr_info("%s: path: %s ret: %d\n", __func__, usermnt, ret);
 }
 #endif
 
@@ -1054,7 +1054,7 @@ static void try_umount(const char *mnt, bool check_mnt, int flags)
 	}
 #endif
 
-#ifdef KSU_HAS_PATH_UMOUNT
+#if LINUX_VERSION_CODE >= KERNEL_VERSION(5, 9, 0) || defined(KSU_HAS_PATH_UMOUNT)
 	ksu_path_umount(mnt, &path, flags);
 #else
 	ksu_sys_umount(mnt, flags);
