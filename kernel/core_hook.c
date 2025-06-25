04d412a7b8670f4b9c729fd8656370f54ffbd5fa
schqiushui <orochi9999@gmail.com>
2025-06-25 22:18:04 +0800

    kernel: fix typos
    Signed-off-by: schqiushui <orochi9999@gmail.com>

--- kernel/core_hook.c
+++ kernel/core_hook.c
@@ -566,26 +566,26 @@ int ksu_handle_prctl(int option, unsigned long arg2, unsigned long arg3,
 
 	// Get SUSFS function status
 	if (arg2 == CMD_GET_SUSFS_FEATURE_STATUS) {
-    	struct susfs_feature_status status;
-    
-    	if (!ksu_access_ok((void __user*)arg3, sizeof(status))) {
-        	pr_err("susfs_feature_status: arg3 is not accessible\n");
-        	return 0;
-    	}
-    
-    	init_susfs_feature_status(&status);
-    
-    	if (copy_to_user((void __user*)arg3, &status, sizeof(status))) {
-        	pr_err("susfs_feature_status: copy_to_user failed\n");
-        	return 0;
-    	}
-    
-    	if (copy_to_user(result, &reply_ok, sizeof(reply_ok))) {
-        	pr_err("susfs_feature_status: prctl reply error\n");
-    	}
-    
-    	pr_info("susfs_feature_status: successfully returned feature status\n");
-    	return 0;
+		struct susfs_feature_status status;
+
+		if (!ksu_access_ok((void __user*)arg3, sizeof(status))) {
+			pr_err("susfs_feature_status: arg3 is not accessible\n");
+			return 0;
+		}
+
+		init_susfs_feature_status(&status);
+
+		if (copy_to_user((void __user*)arg3, &status, sizeof(status))) {
+			pr_err("susfs_feature_status: copy_to_user failed\n");
+			return 0;
+		}
+
+		if (copy_to_user(result, &reply_ok, sizeof(reply_ok))) {
+			pr_err("susfs_feature_status: prctl reply error\n");
+		}
+
+		pr_info("susfs_feature_status: successfully returned feature status\n");
+		return 0;
 	}
 
 #ifdef CONFIG_KSU_SUSFS
